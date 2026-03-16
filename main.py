019335b90680b2be28d2600d55d88f4aea52d381
unknown <camargomateus571@gmail.com>
2026-03-16 07:43:35 -0300

    fix: python main.py with PORT

--- main.py
+++ main.py
@@ -69,3 +69,8 @@ async def spa_handler(request, exc):
 
 if os.path.exists(frontend_dir):
     app.mount('/', StaticFiles(directory=frontend_dir, html=True), name='frontend')
+
+if __name__ == '__main__':
+    import uvicorn
+    port = int(os.getenv('PORT', 8000))
+    uvicorn.run(app, host='0.0.0.0', port=port)
